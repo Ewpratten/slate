88d532f6467501a7e8005cdf928b178c456332cc
Evan Pratten <ewpratten@gmail.com>
2019-10-10 15:32:25 -0400

    Cleanup file loading

--- src/main/java/slate/parser/Command.java
+++ src/main/java/slate/parser/Command.java
@@ -21,7 +21,6 @@ public class Command{
         try {
             //Get file from res folder
             file = new File(Commands.class.getResource("../../commands/CommandUsage.slateinfo").toURI());
-            // file = new File(Commands.class.getResource("commands/CommandUsage.slateinfo").toURI());
 
             //Place all bytes into array
             data = Files.readAllBytes(file.toPath());

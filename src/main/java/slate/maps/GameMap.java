b214eba38533a072020741f725ba735fd569e877
Evan Pratten <ewpratten@gmail.com>
2019-10-21 17:36:52 -0400

    Finished rooms

--- src/main/java/slate/maps/GameMap.java
+++ src/main/java/slate/maps/GameMap.java
@@ -9,13 +9,16 @@ import slate.rooms.Hallway;
 import slate.rooms.Laboratory;
 import slate.rooms.StartRoom;
 import slate.rooms.StorageRoom;
+import slate.rooms.Vault;
 
 public class GameMap extends MapBase {
 
     /* Rooms */
     RoomBase starting_room, start_hall_right, start_hall_left, mini_lab_1, mini_lab_2, mini_lab_storage,
             mini_lab_storage_shared, mini_lab_elevator, large_lab, large_lab_hall, large_lab_storage,
-            large_lab_elevator, large_lab_brk_room, end_hall, end_hall_storage, end_hall_elevator;
+            large_lab_elevator, large_lab_brk_room, end_hall, end_hall_storage, end_hall_elevator, floor2_main_hall,
+            floor2_side_hall, floor2_brk_room, floor2_storage, floor2_front_elevator, floor2_rear_elevator,
+            final_elevator, final_hall, vault;
 
     /**
      * Here we define this map, and it's rooms
@@ -50,8 +53,33 @@ public class GameMap extends MapBase {
         end_hall_storage = new StorageRoom();
         end_hall_elevator = new Elevator();
 
-        /* Linking rooms */
+        // 2nd floor
+        floor2_front_elevator = new Elevator();
+        floor2_main_hall = (new Hallway()).named("Long hallway");
+        floor2_side_hall = (new Hallway()).named("Dark hallway");
+        floor2_brk_room = new BreakRoom();
+        floor2_storage = (new StorageRoom()).named("Locked storage room");
+        floor2_rear_elevator = (new Elevator()).named("Freight elevator");
 
+        // 3rd floor
+        final_elevator = (new Elevator()).named("Freight elevator");
+        final_hall = (new Hallway()).named("Vault hallway");
+        vault = new Vault();
+
+        /* Linking rooms */
+        linkRooms();
+
+        /* Fill rooms with items */
+
+        /* Config navigator */
+        initNavigator();
+
+    }
+
+    /**
+     * All code to link rooms to eachother should go here
+     */
+    private void linkRooms() {
         // Link starting room to it's hallways
         starting_room.addPathway(start_hall_left, start_hall_right);
 
@@ -90,10 +118,34 @@ public class GameMap extends MapBase {
         end_hall_storage.addPathway(end_hall);
 
         // End hall elevator
-        end_hall_elevator.addPathway(end_hall);
+        end_hall_elevator.addPathway(end_hall, floor2_front_elevator);
 
-        /* Fill rooms with items */
+        // Link 2nd floor to first
+        floor2_front_elevator.addPathway(end_hall_elevator, floor2_main_hall);
 
+        // Link 2nd floor main hall
+        floor2_main_hall.addPathway(floor2_front_elevator, floor2_storage, floor2_side_hall);
+        floor2_storage.addPathway(floor2_main_hall);
+
+        // Link 2nd floor side hall
+        floor2_side_hall.addPathway(floor2_main_hall, floor2_brk_room, floor2_rear_elevator);
+        floor2_brk_room.addPathway(floor2_side_hall);
+
+        // Link 3rd floor elevator
+        floor2_rear_elevator.addPathway(floor2_side_hall, final_elevator);
+        final_elevator.addPathway(floor2_rear_elevator, final_hall);
+
+        // Link 3rd floor hallway
+        final_hall.addPathway(final_elevator, vault);
+
+        // Link vault
+        vault.addPathway(final_hall);
+    }
+
+    /**
+     * All navigation code should go here
+     */
+    private void initNavigator() {
         // Set the starting room
         nav.setDefaultRoom(starting_room);
 
@@ -101,7 +153,8 @@ public class GameMap extends MapBase {
         // This is required to let the navigation system know about each room
         registerRooms(starting_room, start_hall_right, start_hall_left, mini_lab_1, mini_lab_2, mini_lab_storage,
                 mini_lab_storage_shared, mini_lab_elevator, large_lab, large_lab_hall, large_lab_storage,
-                large_lab_elevator, large_lab_brk_room, end_hall, end_hall_storage, end_hall_elevator);
+                large_lab_elevator, large_lab_brk_room, end_hall, end_hall_storage, end_hall_elevator, floor2_main_hall,
+                floor2_side_hall, floor2_brk_room, floor2_storage, floor2_front_elevator, floor2_rear_elevator);
     }
 
     @Override
